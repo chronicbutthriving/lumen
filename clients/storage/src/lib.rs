f0ecdb376f09cfba24a60334a5e3deafcb235cc2
Hayden Young <hayden@hayden.moe>
2026-03-31 20:01:23 +0100

    feat: storage client crate

---
+++ clients/storage/src/lib.rs
@@ -0,0 +1,16 @@
+progenitor::generate_api!(
+    spec = "../../openapi/storage/storage-latest.json",
+    interface = Positional,
+    derives = [schemars::JsonSchema, PartialEq],
+    inner_type = slog::Logger,
+    pre_hook = (|log: &slog::Logger, request: &reqwest::Request| {
+        slog::debug!(log, "client request";
+            "method" => %request.method(),
+            "uri" => %request.url(),
+            "body" => ?&request.body(),
+        );
+    }),
+    crates = {
+        "lumen-uuid-kinds" = "*",
+    },
+);
